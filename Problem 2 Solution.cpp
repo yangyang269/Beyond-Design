62ad1240e105bcde8e09abe3f41b8cb3d3c499b8
yangyang269 <113395548+yangyang269@users.noreply.github.com>
2024-05-31 22:06:19 -0400

    Add files via upload

---
+++ Problem 2 Solution.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main() {
+    // Declare and initialize four string variables
+    std::string wordOne = "I";
+    std::string wordTwo = "enjoy";
+    std::string wordThree = "learning";
+    std::string wordFour = "C++";
+
+    // Print each word on a separate line
+    std::cout << wordOne << std::endl;
+    std::cout << wordTwo << std::endl;
+    std::cout << wordThree << std::endl;
+    std::cout << wordFour << std::endl;
+}
