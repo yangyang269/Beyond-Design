8d3915a0bab2b59411f3eccbeee8c34d306d104d
yangyang269 <113395548+yangyang269@users.noreply.github.com>
2024-05-31 21:53:54 -0400

    Add files via upload

---
+++ Problem 1 Solution.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+int main() {
+    // Declare and initialize three integer variables
+    int variableOne = 10;
+    int variableTwo = 20;
+    int variableThree = 30;
+
+    // Print the initial values of the variables
+    std::cout << "Initial values:" << std::endl;
+    std::cout << "variableOne: " << variableOne << std::endl;
+    std::cout << "variableTwo: " << variableTwo << std::endl;
+    std::cout << "variableThree: " << variableThree << std::endl;
+
+    // Change the values of the variables
+    variableOne = 40;
+    variableTwo = 50;
+    variableThree = 60;
+
+    // Print the new values of the variables
+    std::cout << "New values:" << std::endl;
+    std::cout << "variableOne: " << variableOne << std::endl;
+    std::cout << "variableTwo: " << variableTwo << std::endl;
+    std::cout << "variableThree: " << variableThree << std::endl;
+}
